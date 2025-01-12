7ac0bb03d17774170947d145ed28da7613f6e659
INNOVTERRAJ <singhrajaryan83@gmail.com>
2025-01-13 00:02:27 +0530

    modified

--- backend/routes/numerology.js
+++ backend/routes/numerology.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("./models/user");
 
 // Route to handle numerology predictions
 router.post("/", async (req, res) => {
